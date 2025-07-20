2b165341cc8befc3648a14848ed49ebaa4add120
sreejan <sreejandas226@gmail.com>
2025-07-20 22:58:37 +0530

    sectional fixes

--- apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -1,14 +1,32 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const HeaderBottom = () => {
 
     const [show, setShow] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
 
+
+    //Task scroll position 
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if(window.screenY > 100){
+          setIsSticky(true);
+        }
+        else {
+          setIsSticky(false);
+        }
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll)
+    },[]);
+
+    
+
   return (
-    <div>
+    <div className=''>
       
     </div>
   )
